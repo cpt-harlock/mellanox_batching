c8419f04a4f04cf45f19667349d93cf26081d8ed
cpt-harlock <andrea.monterubbiano@gmail.com>
2024-10-14 18:30:14 +0200

    added utility to configure metadata

--- mlx5/core/en_ioctl.h
+++ mlx5/core/en_ioctl.h
@@ -1,4 +1,4 @@
 #include <linux/ioctl.h>
 
-#define SET_METADATA _IOW('mlx5_ioctl','set',int32_t*)
-#define RESET_METADATA _IOW('mlx5_ioctl','reset',int32_t*)
+#define SET_METADATA _IO('mlx5_ioctl','set')
+#define RESET_METADATA _IO('mlx5_ioctl','reset')
